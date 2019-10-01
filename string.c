bc544554ec0ebd464dfccf6e2ed903416fefcf8b
ZverevKA <kostya-zverev9@yandex.ru>
2019-10-02 03:38:34 +0500

    string

---
+++ string.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+
+int newStrlen(char str[])
+{
+	int k = 0;
+	while (str[k]!='\0')
+	{
+		k++;
+	}
+	return k;
+}
+
+void newStrcpy(char outstr[], char instr[])
+{
+	int k = 0;
+	while (instr[k]!='\0')
+	{
+		outstr[k] = instr[k];
+		k++;	
+	}
+	outstr[k] = instr[k];
+}	
+
+void newStrcat(char outstr[], char instr[])
+{
+	int k = newStrlen(outstr);
+	for (int i = 0; i <= newStrlen(instr); i++)
+	{
+		outstr[k+i] = instr[i];
+	}
+}
+
+int newStrcmp(char str1[], char str2[])
+{
+	int k,t = newStrlen(str1);
+	if (newStrlen(str1) < newStrlen(str2))
+	{
+		t = newStrlen(str2);
+	}
+	for (int i = 0; i <= t; i++)
+	{
+		k = str1[i] - str2[i];
+		if (k != 0)
+		{
+			break;
+		} 
+	}
+return k;
+}
+
+
