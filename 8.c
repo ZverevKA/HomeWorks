5ca0ce660bf8142e73b415cc77e9b5afae386ec2
ZverevKA <55195614+ZverevKA@users.noreply.github.com>
2019-10-02 03:02:51 +0500

    Update 8.c
    Added correct memory allocation

--- 8.c
+++ 8.c
@@ -25,17 +25,29 @@ void main()
 	printf("Enter number of signs in \"long\" string(S)\n");
 	scanf("%d", &N);
 	printf("Enter \"long\" string(S)\n");
-	S = malloc(N * sizeof(char));
+	S = (char *)malloc(N * sizeof(char));
+	if (S == NULL)
+	{
+		printf("Memory ERROR");
+		return -1;
+	}
 	scanf("%s", S);
 	printf("Enter number of signs in \"short\" string(S1)\n");
 	scanf("%d", &N1);
 	printf("Enter \"short\" string(S1)\n");
-	S1 = malloc(N1 * sizeof(char));
+	S1 = (char *)malloc(N1 * sizeof(char));
+	if (S1 == NULL)
+	{
+		printf("Memory ERROR");
+		return -1;
+	}
 	scanf("%s", S1);
 	for (int i = 0; i <= N - N1; i++)
 	{
 		k += includingCheck(S, S1, N1, i);
 	}
 	printf("S1 include in S %d times", k);
+	free(S);
+	free(S1);
 }	
 	
