0af7160b4e03c2e62e9801f86d8ea82f9241c29c
ZverevKA <kostya-zverev9@yandex.ru>
2019-12-03 22:32:15 +0500

    Source

---
+++ listNode.c
@@ -0,0 +1,139 @@
+#include "listNode.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+List* createList()
+{
+List *list = malloc(sizeof(List));
+	list->size = 0;
+	list->head = NULL;
+	list->tail = NULL;
+return list;
+}
+
+List* addHeadNode(List *list, int val)
+{
+	Node *node = malloc(sizeof(Node));
+	node->next = list->head;
+	node->val = val;
+	list->size++;
+	list->head = node;
+	if (list->tail == NULL)
+	{
+		list->tail = node;
+	}
+	
+	return list;
+}
+
+List* addTailNode(List *list, int val)
+{
+	Node *node = malloc(sizeof(Node));
+	node->next = NULL;
+	node->val = val;
+	list->size++;
+	list->tail = node;
+	if (list->head = NULL)
+	{
+		list->head = node;
+	}
+	return list;
+}
+
+List* addAfterNode(List *list, Node *node, int val)
+{
+	Node *newNode = malloc(sizeof(Node));
+	newNode->val = val;
+	newNode->next = node->next;
+	node->next = newNode;
+	list->size++;
+	if (list->tail == node)
+	{
+		list->tail = newNode;
+	}
+	return list;
+}
+
+Node* deleteHead(List *list)
+{
+	Node *node = list->head;
+	if (list->tail == node)
+	{
+		list->tail = NULL;
+	}
+	list->head = node->next;
+	list->size--;
+	free(node);
+	return list->head;
+}
+
+Node* deleteNoHead(List *list, Node *lastNode)
+{
+	Node *node = lastNode->next;
+	if (list->tail = node)
+	{
+		list->tail = lastNode;
+	}
+	lastNode->next = node->next;
+	list->size--;
+	free(node);
+	return lastNode->next;
+}
+
+List* deleteVal(List *list, int val)
+{
+	Node *node = list->head;
+	Node *lastNode = NULL;
+	while(node != NULL)
+	{
+		if (node->val == val)
+		{
+			if (list->head == node)
+			{
+				node = deleteHead(list);
+				lastNode = node;
+			}
+			else 
+			{
+			node = deleteNoHead(list, lastNode);
+			}
+		}
+		else 
+		{
+			lastNode = node;
+			node = node->next;
+		}
+	}
+	return list;
+}
+	
+void printList(List *list)
+{
+	Node *node = list->head;
+	if (node == NULL)
+	{
+		printf("List is clear\n");
+	}
+	else
+	{
+	while (node != NULL)
+	{
+		printf("%d\n", node->val);
+		node = node->next;
+	}
+	}
+}
+
+void clearList(List *list)
+{
+	Node *node = list->head;
+	Node *node1 = list->head;
+	while (node != NULL)
+	{
+		node1 = node;
+		node = node1->next;
+		free(node1);
+	}
+	free(list);
+}
+
---
+++ listNode.h
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+
+typedef struct Node1
+{
+	int val;
+	struct Node1 *next;
+} Node;
+
+typedef struct List1
+{
+	size_t size;
+	Node *head;
+	Node *tail;
+} List;
+
+
+List* createList();
+List* addHeadNode(List *list, int val);
+List* addTailNode(List *list, int val);
+List* addAfterNode(List *list, Node *node, int val);
+List* deleteVal(List *list, int val);
+void printList(List *list);
+void clearList(List *list);
+
+
